213660c4774472f7bfa21c3e33d3f12215996209
Emir Nakle <emirnakle@gmail.com>
2018-08-08 20:05:58 -0300

    [WIP]

---
+++ Assets/Script/game/Poderes/Tierra.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Tierra : CAnimatedSprite
+{
+    public Tierra()
+    {
+        setFrames(Resources.LoadAll<Sprite>("Sprites/tierra"));
+       
+        setName("PoderTierra");
+        setSortingLayerName("Player");
+        setScale(2);
+ 
+    }
+
+
+
+    override public void update()
+    {
+        base.update();
+    }
+
+    override public void render()
+    {
+        base.render();
+    }
+
+    override public void destroy()
+    {
+        base.destroy();
+    }
+
+
+
+   
+}
